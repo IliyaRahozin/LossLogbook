b8e77ea45f4d36a6f63d3d2067b6b3fe0bd9fe1a
Illia Rahozin <yarusha4true@gmail.com>
2023-08-23 00:04:38 +0300

    Basic project setting

---
+++ LossLogbook/LossLogbook/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LossLogbook
+//
+//  Created by Illia Rahozin on 22.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
